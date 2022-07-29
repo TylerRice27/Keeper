e5a90ce151ba6dbfa9b22b184ba517b66a9b0b70
TylerRice27 <Tylerrayrice2@gmail.com>
2022-07-29 14:31:38 -0600

    Made all backend files and transients

--- Keeper/Startup.cs
+++ Keeper/Startup.cs
@@ -1,17 +1,10 @@
- using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using MySqlConnector;
 using Keeper.Repositories;
@@ -39,9 +32,18 @@ namespace Keeper
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Keeper", Version = "v1" });
             });
             services.AddScoped<IDbConnection>(x => CreateDbConnection());
-            
+
             services.AddScoped<AccountsRepository>();
             services.AddScoped<AccountService>();
+
+            services.AddTransient<VaultsRepository>();
+            services.AddTransient<VaultsService>();
+
+            services.AddTransient<KeepsRepository>();
+            services.AddTransient<KeepsService>();
+
+            services.AddTransient<VaultKeepsRepository>();
+            services.AddTransient<VaultKeepsService>();
         }
 
         private void ConfigureCors(IServiceCollection services)
@@ -94,10 +96,10 @@ namespace Keeper
             }
 
             app.UseHttpsRedirection();
-            
+
             app.UseDefaultFiles();
             app.UseStaticFiles();
-            
+
             app.UseRouting();
 
             app.UseAuthentication();
